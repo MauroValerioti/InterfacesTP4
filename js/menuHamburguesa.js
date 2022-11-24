6dca953c47df2b40b9d733be43ca3b97adbf44a2
Mauro <mauro.valerioti.18@gmail.com>
2022-11-24 12:57:28 -0300

    esto

--- js/menuHamburguesa.js
+++ js/menuHamburguesa.js
@@ -15,14 +15,13 @@ var itemMenuDesplegable3 = document.querySelector(".itemMenuDesplegable3");
 var itemMenuDesplegable4 = document.querySelector(".itemMenuDesplegable4");
 var itemMenuDesplegable5 = document.querySelector(".itemMenuDesplegable5");
 var itemMenuDesplegable6 = document.querySelector(".itemMenuDesplegable6");
+
 //animacion sobre las barras.
 function animateBars(){
     line1__bars.classList.toggle("activeline1__bars-menu");
     line2__bars.classList.toggle("activeline2__bars-menu");
     line3__bars.classList.toggle("activeline3__bars-menu");
-
     menuDesplegable.classList.toggle("activarMenuDesplegable");
-
     itemMenuDesplegable1.classList.toggle("ActivarItemMenuDesplegable1");
     itemMenuDesplegable2.classList.toggle("ActivarItemMenuDesplegable2");
     itemMenuDesplegable3.classList.toggle("ActivarItemMenuDesplegable3");

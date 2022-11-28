93cc84685d3ea88994299fb38f76ae3ea3b7c455
Mauro <mauro.valerioti.18@gmail.com>
2022-11-28 15:29:57 -0300

    inciso 5

---
+++ js/fondoAnimado.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll',function(){
+    let animacion = document.querySelector('.fondoAnimado');
+    let positionObj1 = animacion.getBoundingClientRect().top;
+    
+    
+    console.log(Math.round(positionObj1));
+    
+
+    if(positionObj1 < 600 && positionObj1 > -20 ){
+        document.querySelector('.imagen_muneco').classList.toggle("animarPoppy");
+        document.querySelector('.tituloAnimado1').classList.toggle("animarTitulo");
+        document.querySelector('.tituloAnimado2').classList.toggle("animarTitulo");
+    }
+})
